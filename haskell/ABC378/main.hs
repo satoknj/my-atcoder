c6cb78a082d40f4b98beee438fdd1d99e42129ee
satoknj <gk0909c@gmail.com>
2024-12-04 06:18:14 +0900

    リファクタリング
    - IO との fmap をアプリカティブファンクタの中置演算子に変更

--- haskell/ABC378/main.hs
+++ haskell/ABC378/main.hs
@@ -4,17 +4,21 @@ import Data.Foldable (for_)
 
 main :: IO ()
 main = do
-  n <- fmap read getLine :: IO Int
+  -- get input
+  n <- read <$> getLine
   qrs <- replicateM n getLine
-  j <- fmap read getLine :: IO Int
+  j <- read <$> getLine
   tds <- replicateM j getLine
 
+  -- transform
   let digits = map words qrs
   let nums = map (map read) digits
   let qrArray = listArray (1, n) nums :: Array Int [Int]
 
+  -- solve
   for_ tds $ \tdStr -> do
     let td = map read  (words tdStr) :: [Int]
+
     let t = head td
     let d = td !! 1
     let qr = qrArray ! t :: [Int]
