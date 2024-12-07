b8450e663a400493d6bfdf37732be30923ff00cf
satoknj <gk0909c@gmail.com>
2024-12-08 06:41:18 +0900

    Tuple を使うように変更

--- haskell/ABC378/main.hs
+++ haskell/ABC378/main.hs
@@ -2,31 +2,21 @@ import Control.Monad (replicateM)
 import Data.Array (listArray, Array, (!))
 import Data.Foldable (for_)
 
--- todo
--- 現状理解
--- タプルを使う
-
-getInts :: IO [Int]
-getInts =  map read.words <$> getLine
+getIntTuple :: IO (Int, Int)
+getIntTuple = (\[x,y] -> (x, y)).map read.words <$> getLine
 
 main :: IO ()
 main = do
   -- get input
   n <- read <$> getLine
-  qrs <- replicateM n getInts
+  qrs <- replicateM n getIntTuple
   j <- read <$> getLine
-  tds <- replicateM j getInts
+  tds <- replicateM j getIntTuple
 
   -- transform
-  let qrArray = listArray (1, n) qrs :: Array Int [Int]
+  let qrArray = listArray (1, n) qrs :: Array Int (Int, Int)
 
   -- solve
-  for_ tds $ \td -> do
-    let t = head td
-    let d = td !! 1
-
-    let qr = qrArray ! t :: [Int]
-    let q = head qr
-    let r = qr !! 1
-
+  for_ tds $ \(t, d) -> do
+    let (q, r) = qrArray ! t :: (Int, Int)
     print $ d + (q - (d-r)) `mod` q
