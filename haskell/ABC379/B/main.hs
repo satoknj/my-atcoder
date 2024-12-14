58b113a1f1420eb62404dd40aae9e619c5425cfe
satoknj <gk0909c@gmail.com>
2024-12-15 06:11:22 +0900

    余計な出力、型アノテーションを削除

--- haskell/ABC379/B/main.hs
+++ haskell/ABC379/B/main.hs
@@ -1,14 +1,9 @@
 import Data.List (group)
 
 main = do
-  [n,k] <- map (read :: String -> Int) . words <$> getLine
+  [n,k] <- map read . words <$> getLine
   s <- getLine
-  print n
-  print k
-  print s
 
   let a = [(head x, length x) | x <- group s]
-  let b = filter (\x -> snd x > k && fst x == 'O') a
+  let b = filter (\x -> snd x >= k && fst x == 'O') a
   print $ (sum . map ((`div` k) . snd)) b
-
-  print "hoge"
