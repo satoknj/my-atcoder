28cc43c5742a77dd1efc435cd2e1418aa014ab44
satoknj <gk0909c@gmail.com>
2024-12-21 06:49:02 +0900

    doctest を使ってみた

--- haskell/ABC379/B/main.hs
+++ haskell/ABC379/B/main.hs
@@ -4,6 +4,15 @@ main = do
   [n,k] <- map read . words <$> getLine
   s <- getLine
 
+  print $ rle' n k s
+
+-- | my rle
+-- >>> rle' 7 3 "OOXOOOO"
+-- 1
+-- >>> rle' 12 2 "OXXOOOXOOOOX"
+-- 3
+rle' :: Int -> Int -> String -> Int
+rle' n k s = do
   let a = [(head x, length x) | x <- group s]
   let b = filter (\x -> snd x >= k && fst x == 'O') a
-  print $ (sum . map ((`div` k) . snd)) b
+  (sum . map ((`div` k) . snd)) b
