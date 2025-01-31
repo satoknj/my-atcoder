13650b93aecd64266aaafdacf0c562b1daf9e31c
satoknj <gk0909c@gmail.com>
2025-02-01 06:55:28 +0900

    パターンマッチを使って解くように書き直し

--- haskell/ABC382/b.hs
+++ haskell/ABC382/b.hs
@@ -5,31 +5,15 @@ main = do
   nd <- getLine
   s <- getLine
   let [n,d] = (map read . words) nd
-  putStrLn $ solve s n d
+  let revS = reverse s
+  let f = reverse . solve'' d
+  putStrLn $ f revS
 
 -- |
--- >>> solve ".@@.@" 5 2
--- ".@..."
--- >>> solve "@@@" 3 3
--- "..."
-solve :: String -> Int -> Int -> String
-solve s n d = 
-  let indiceis = elemIndices '@' s
-  in
-    if null indiceis
-    then
-      fs
-    else
-      let a = take (length indiceis - d) indiceis
-        in
-          if null a
-          then
-            fs
-          else
-            let b = last a
-              in
-                let c = b + 1
-                in
-                  take c s ++ replicate (n-c) '.'
-    where
-      fs = replicate n '.'
+-- >>> solve'' 1 "@.@@."
+-- "..@@."
+solve'' :: Int -> String -> String
+solve'' d [] = []
+solve'' d ('.':xs) = '.' : solve'' d xs
+solve'' 0 (x:xs) = x : solve'' 0 xs
+solve'' d (x:xs) = '.' : solve'' (d-1) xs
